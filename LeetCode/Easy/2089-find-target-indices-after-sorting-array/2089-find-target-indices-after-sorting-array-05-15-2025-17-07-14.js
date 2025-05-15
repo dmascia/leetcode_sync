a39d0407539afac0e7600b109becaabb6a9f6d78
David Mascia <753866+dmascia@users.noreply.github.com>
2025-05-15 17:07:14 -0400

    Time: 2 ms (67.33%), Space: 54.5 MB (56.14%) - LeetHub

---
+++ LeetCode/Easy/2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array-05-15-2025-17-07-14.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var targetIndices = function(nums, target) {
+    const sortedNums = nums.sort((a,b) => a - b);
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) result.push(i);
+    }
+
+    return result;
+};
